65ac5036dd2966c3f66c711bcf35957d93cd5d39
F <f@s.o>
2017-12-18 14:06:34 +0100

    add icomoon icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import { AutoInputLengthDirective } from './auto-input-length.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodolistComponent
+    TodolistComponent,
+    AutoInputLengthDirective
   ],
   imports: [
     BrowserModule,
