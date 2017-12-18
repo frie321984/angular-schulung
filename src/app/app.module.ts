18783c27f01e55be4619a68c04353385789d8b38
F <f@s.o>
2017-12-18 17:00:17 +0100

    chucknorris...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,28 @@
-import { GlobalErrorHandler } from './GlobalErrorHandler';
+import { ChucknorrisService } from './chucknorris.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { AutoInputLengthDirective } from './auto-input-length.directive';
+import { JokeComponent } from './joke/joke.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TodolistComponent,
-    AutoInputLengthDirective
+    AutoInputLengthDirective,
+    JokeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [
-    {
-      provide: ErrorHandler,
-      useClass: GlobalErrorHandler
-    }
-  ],
+  providers: [ChucknorrisService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
