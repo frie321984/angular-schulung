714f4a0f43724b0d6ebbc57dabd8091d981cb720
F <f@s.o>
2017-12-19 11:17:32 +0100

    Router eingebunden

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { ChucknorrisService } from './chucknorris.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { AutoInputLengthDirective } from './auto-input-length.directive';
 import { JokeComponent } from './joke/joke.component';
-import { CrazycasePipe } from './todolist/crazycase.pipe';
+import { CrazycasePipe } from './shared/listbox/crazycase.pipe';
+import { ListboxComponent } from './shared/listbox/listbox.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
@@ -17,12 +19,22 @@ import { CrazycasePipe } from './todolist/crazycase.pipe';
     TodolistComponent,
     AutoInputLengthDirective,
     JokeComponent,
-    CrazycasePipe
+    CrazycasePipe,
+    ListboxComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: TodolistComponent
+    },
+  {
+    path: 'about',
+    component: AboutComponent
+  }])
   ],
   providers: [ChucknorrisService],
   bootstrap: [AppComponent]
